9915fd85a1ce7914073b7505a75a46ea86f45511
Barry Peddycord III <ishara@isharacomix.org>
2014-12-10 12:05:59 -0500

    Got my name and such showing up now.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,19 @@
-$("#main").append("Barry Peddycord III");
+var skills = ["Python","Javascript","Django"];
+var bio = {
+  "name": "Barry Peddycord III",
+  "role": "Research Assistant",
+  "pictureURL": "http://isharacomix.org/img/barry_avatar.png",
+  "welcome": "Hello world!",
+  "skills": skills
+}
+
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+
+var formattedPic = HTMLbioPic.replace("%data%",bio.pictureURL);
+var formattedWelcome = HTMLWelcomeMsg.replace("%data",bio.welcome);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedPic);
